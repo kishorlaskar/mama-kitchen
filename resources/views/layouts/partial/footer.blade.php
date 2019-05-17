9c29693a8b692e57c8000ff64266b2cb1b57c80a
Kishor Laskar <30683532+kishorlaskar@users.noreply.github.com>
2019-05-17 23:23:52 +0600

    It is a online resturant system
    It's a basic resturent site where you can order

---
+++ resources/views/layouts/partial/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <p class="copyright pull-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>
+            Design by <a href="http://www.creative-tim.com">Creative Tim</a>, and Developed by <a href="https://cipfahim.github.io/">Md.Aminul Islam Fahim</a>
+        </p>
+    </div>
+</footer>
